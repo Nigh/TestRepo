8a9a2da0ca1e67af8e8b480475dbd324f6f618d4
Nigh <jiyucheng007@gmail.com>
2014-12-12 16:55:31 +0800

    休眠延时增长至60 sec
    Signed-off-by: Nigh <jiyucheng007@gmail.com>

--- lib/project.h
+++ lib/project.h
@@ -15,7 +15,7 @@
 #define NECK_ROLL_NEGATIVE_UPLIMIT (-5)
 #define NECK_ROLL_NEGATIVE_DOWNLIMIT (-45)
 
-#define G_ACTIVE_TIMEOUT (300)	// x 0.1sec
+#define G_ACTIVE_TIMEOUT (600)	// x 0.1sec
 #define STEP_ACTIVE_TIMEOUT (50)	// x 0.1sec
 
 #define BLE_CONNECT_TIMEOUT_SET (10)	// x 1sec
