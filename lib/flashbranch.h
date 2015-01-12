7608920e2c97fc7550483c9e6c2b89822deddcaf
Nigh <jiyucheng007@gmail.com>
2015-01-12 21:28:28 +0800

    first commit
    Signed-off-by: Nigh <jiyucheng007@gmail.com>

--- lib/flashbranch.h
+++
@@ -1,9 +0,0 @@
-#ifndef		__FLASHBRANCH_H
-#define		__FLASHBRANCH_H
-
-#pragma ext_func selfprogramfunc 0
-#pragma ext_func R_MAIN_SelfWrite 1
-#pragma ext_func FlashCommand	2
-#pragma ext_func spi_flash_read 3
-
-#endif
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Empty repository
+
+## For Test
+
+Tempora mutantur nos et mutamur in illis.
